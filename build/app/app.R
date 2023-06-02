52773da69bb520f7d564f55ba0d21b23112b76df
Justin Marciszewski <justinjm@google.com>
2023-06-02 14:07:56 -0400

    initial commit
    * fork/clone of chatgpt app to get a UI and UX baseline (not for chat gpt integration)

--- build/app/app.R
+++ build/app/app.R
@@ -1,86 +1,100 @@
 library(shiny)
-library(shinydashboard)
-library(shinydashboardPlus)
-library(shinythemes)
-library(DT)
-library(ggplot2)
+library(httr)
+library(stringr)
 
 ui <- fluidPage(
-  # theme = shinytheme("cyborg"),
-  navbarPage(
-    title = "Shiny App Demo",
-    windowTitle = "Shiny App Demo",
-    id = "tabactive",
-    tabPanel("Main", 
-             icon = icon("table"),
-             tags$body(
-               dashboardPage(
-                 dashboardHeader(title = "Main", disable = TRUE),
-                 dashboardSidebar(
-                   width = "250",
-                   sidebarMenu(id = "sidebarmenu",
-                               menuItem("Basic DataTable", 
-                                        tabName = "menu1", 
-                                        icon = icon("chart-line"))
-                   ),
-                   minified = FALSE
-                 ),
-                 dashboardBody(
-                   tabItems(
-                     tabItem(tabName = "menu1", 
-                             # Create a new Row in the UI for selectInputs
-                             fluidRow(
-                               column(4,
-                                      selectInput("man",
-                                                  "Manufacturer:",
-                                                  c("All",
-                                                    unique(as.character(mpg$manufacturer))))
-                               ),
-                               column(4,
-                                      selectInput("trans",
-                                                  "Transmission:",
-                                                  c("All",
-                                                    unique(as.character(mpg$trans))))
-                               ),
-                               column(4,
-                                      selectInput("cyl",
-                                                  "Cylinders:",
-                                                  c("All",
-                                                    unique(as.character(mpg$cyl))))
-                               )
-                             ),
-                             # Create a new row for the table.
-                             DT::dataTableOutput("table")
-                     )
-                     
-                   )
-                 )
-               )
-             )
+  div(
+    titlePanel("Shiny App Demo"),
+    style = "color: white; background-color: #4285f4"
+  ),
+  sidebarLayout(
+    sidebarPanel(
+      h3("Welcome to a Demo Shiny App!"),
+      p("This application allows you summarize text, fill out the inputs below and then ask a question to the right."),
+      textInput("user_name", "User Name", "<FIRST NAME, LAST NAME>"),
+      tags$p("Learn more about BigQuery ", 
+             tags$a(href = "https://cloud.google.com/bigquery/docs", target="_blank", "here")
+      ),tags$hr(),
+      selectInput("user_type", "User Type",
+                  choices = c("New", "Existing"), selected = "new"),
+      tags$hr(),
+      sliderInput("temperature", "Temperature", min = 0.1, max = 1.0, value = 0.7, step = 0.1),
+      sliderInput("max_length", "Maximum Length", min = 1, max = 2048, value = 512, step = 1),
+      tags$hr(),
+      textAreaInput(inputId = "sysprompt", label = "SYSTEM PROMPT",height = "200px", placeholder = "You are a helpful assistant."),
+      tags$hr(),
+      tags$div(
+        style="text-align:center; margin-top: 15px; color: white; background-color: #FFFFFF",
+        a(href="https://github.com/justinjm/google-cloud-run-r-shiny", target="_blank",
+          img(src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png", height="30px"),
+          "View source code on Github"
+        )
+      ),
+      style = "background-color: #1a1b1f; color: white"
     )
-  )
-  
-)
+    ,
+    mainPanel(
+      tags$style(type = "text/css", ".shiny-output-error {visibility: hidden;}"),
+      tags$style(type = "text/css", ".shiny-output-error:before {content: ' Check your inputs or API key';}"),
+      tags$style(type = "text/css", "label {font-weight: bold;}"),
+      fluidRow(
+        column(12,tags$h3("Chat History"),tags$hr(),uiOutput("chat_history"),tags$hr())
+      ),
+      fluidRow(
+        column(11,textAreaInput(inputId = "user_message", placeholder = "Enter your message:", label="USER PROMPT", width = "100%")),
+        column(1,actionButton("send_message", "Send",icon = icon("play"),height = "350px"))
+      ),style = "background-color: #00A67E")
+  ),style = "background-color: #3d3f4e")
 
-server <- shinyServer(function(input, output, session){
+server <- function(input, output, session) {
+  chat_data <- reactiveVal(data.frame())
   
-  
-  # Filter data based on selections
-  output$table <- DT::renderDataTable(DT::datatable({
-    data <- mpg
-    if (input$man != "All") {
-      data <- data[data$manufacturer == input$man,]
+  observeEvent(input$send_message, {
+    if (input$user_message != "") {
+      new_data <- data.frame(source = "User", message = input$user_message, stringsAsFactors = FALSE)
+      chat_data(rbind(chat_data(), new_data))
+      
+      gpt_res <- call_gpt_api(input$api_key, input$user_message, input$model_name, input$temperature, input$max_length, input$sysprompt)
+      
+      if (!is.null(gpt_res)) {
+        gpt_data <- data.frame(source = "ChatGPT", message = gpt_res, stringsAsFactors = FALSE)
+        chat_data(rbind(chat_data(), gpt_data))
+      }
+      updateTextInput(session, "user_message", value = "")
     }
-    if (input$cyl != "All") {
-      data <- data[data$cyl == input$cyl,]
-    }
-    if (input$trans != "All") {
-      data <- data[data$trans == input$trans,]
-    }
-    data
-  }))
+  })
   
+  call_gpt_api <- function(api_key, prompt, model_name, temperature, max_length, sysprompt) {
+    response <- httr::POST(
+      url = "https://api.openai.com/v1/chat/completions", 
+      add_headers(Authorization = paste("Bearer", api_key)),
+      content_type("application/json"),
+      encode = "json",
+      body = list(
+        model = model_name,
+        messages = list(
+          list(role = "user", content = prompt),
+          list(role = "system", content = sysprompt)
+        ),
+        temperature = temperature,
+        max_tokens = max_length
+      )
+    )
+    return(str_trim(content(response)$choices[[1]]$message$content))
+  }
   
-})
-
+  output$chat_history <- renderUI({
+    chatBox <- lapply(1:nrow(chat_data()), function(i) {
+      tags$div(class = ifelse(chat_data()[i, "source"] == "User", "alert alert-secondary", "alert alert-success"),
+               HTML(paste0("<b>", chat_data()[i, "source"], ":</b> ", chat_data()[i, "message"])))
+    })
+    do.call(tagList, chatBox)
+  })
+  
+  observeEvent(input$download_button, {
+    if (nrow(chat_data()) > 0) {
+      session$sendCustomMessage(type = "downloadData", message = "download_data")
+    }
+  })
+}
 shinyApp(ui = ui, server = server)

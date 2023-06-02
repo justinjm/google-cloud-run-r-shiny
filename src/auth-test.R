fa85105c548f19fedcf7d39bccdd62f9625c705a
Justin Marciszewski <justinjm@google.com>
2023-06-02 14:48:51 -0400

    test auth

---
+++ src/auth-test.R
@@ -0,0 +1,18 @@
+library(googleAuthR)
+library(googleCloudVertexAIR)
+
+
+projectId <- Sys.getenv("PROJECT_ID") 
+gcva_region_set(region = Sys.getenv("REGION"))
+gcva_project_set(projectId = projectId)
+
+options(googleAuthR.scopes.selected = "https://www.googleapis.com/auth/cloud-platform")
+
+gar_auth(email = Sys.getenv("GAR_AUTH_EMAIL"))
+
+result <- gcva_text_gen_predict(
+  prompt="Give me ten interview questions for the role of a Python software engineer.",
+  modelId="text-bison"
+)
+
+cat(result)

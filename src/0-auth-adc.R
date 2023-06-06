7ef7579e28e79534a184eb66347f89c0e9cfa0ae
Justin Marciszewski <justinjm@google.com>
2023-06-06 11:31:58 -0400

    e2e example for all 3 packages with ADC

--- src/0-auth-adc.R
+++ src/0-auth-adc.R
@@ -1,39 +1,41 @@
 
 # load packages -----------------------------------------------------------
 library(gargle)
+library(googleCloudStorageR)
 library(bigrquery)
-# TODO - figure out ADC
-# library(googleCloudStorageR)
-## TODO - package needs auth migration from googleAuthR to use ADC
-# library(googleCloudVertexAIR)
+library(googleCloudVertexAIR)
 
 # set options and constants -----------------------------------------------
-options(gargle_verbosity = "debug")
-
 project_id <- Sys.getenv("PROJECT_ID")
+# options(googleAuthR.verbose = 2)
+# options(gargle_verbosity = "debug")
 
 # authenticate ------------------------------------------------------------
-credentials_app_default(scopes="https://www.googleapis.com/auth/cloud-platform")
+## workaround for googleCloudStorage auth: 
+## https://github.com/r-lib/gargle/issues/130
+token <- credentials_app_default(scopes="https://www.googleapis.com/auth/cloud-platform")
+gcs_auth(token = token)
 
 # test connections --------------------------------------------------------
 ## List gcs buckets
-# buckets <- gcs_list_buckets(project_id)
-# buckets
+buckets <- gcs_list_buckets(project_id)
+buckets
 
 ## List bigquery datasets 
 datasets <- bq_project_datasets(project_id)
 datasets
 
 # list vertex ai datasets  
-# gcva_list_datasets(projectId = Sys.getenv("PROJECT_ID"),
-#                    locationId = Sys.getenv("REGION"))
+vertex_datasets <- gcva_list_datasets(projectId = Sys.getenv("PROJECT_ID"),
+                                      locationId = Sys.getenv("REGION"))
+vertex_datasets
 
 # Call Vertex LLM API  
-# result <- gcva_text_gen_predict(
-#   projectId = Sys.getenv("PROJECT_ID"),
-#   locationId = Sys.getenv("REGION"),
-#   prompt="Give me ten interview questions for the role of a Python software engineer.",
-#   modelId="text-bison"
-# )
-# 
-# cat(result)
+result <- gcva_text_gen_predict(
+  projectId = Sys.getenv("PROJECT_ID"),
+  locationId = Sys.getenv("REGION"),
+  prompt="Give me ten interview questions for the role of a Python software engineer.",
+  modelId="text-bison"
+)
+
+cat(result)

891557097d445f434ae1ac528f3a0d4f52f0a7e2
Justin Marciszewski <justinjm@google.com>
2023-06-06 17:04:14 -0400

    adding

--- dev/gcp-auth/app.R
+++ dev/gcp-auth/app.R
@@ -12,7 +12,7 @@ cat(scopes <- "https://www.googleapis.com/auth/cloud-platform")
 
 # authenticate ------------------------------------------------------------
 # Add to `.Renviron``: `GARGLE_SVC_ACCT_JSON="path/to/servicetoken.json"`
-# bq_auth(path = Sys.getenv("GARGLE_SVC_ACCT_JSON"))
+bq_auth(path = Sys.getenv("GARGLE_SVC_ACCT_JSON"))
 
 ui <- fluidPage(
   textAreaInput("query", "SQL query"),

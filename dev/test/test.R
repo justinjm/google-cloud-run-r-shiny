9383886a837934ed9fccc0ec9f085f577554961c
Justin Marciszewski <justinjm@google.com>
2023-06-07 14:15:10 -0400

    test successful

--- dev/test/test.R
+++ dev/test/test.R
@@ -4,12 +4,9 @@ library(bigrquery)
 library(DT)
 
 # Define your BigQuery project and dataset
-# project_id <- "your-project-id"
-# dataset <- "your-dataset"
 cat("project_id:", project_id <- Sys.getenv("PROJECT_ID"), "\n")
 cat("dataset_id:", dataset_id <- Sys.getenv("DATASET_ID"), "\n")
 credentials_app_default(scopes="https://www.googleapis.com/auth/cloud-platform")
-
 
 # UI
 ui <- fluidPage(
@@ -34,7 +31,7 @@ server <- function(input, output) {
   
   # Function to query BigQuery and retrieve results
   queryBigQuery <- function(query) {
-    query_job <- bq_project_query(project = project_id, query = query)
+    query_job <- bq_project_query(project_id, query = query)
     bq_table <- bq_table_download(query_job)
     return(bq_table)
   }
@@ -62,7 +59,6 @@ server <- function(input, output) {
   })
   
   # Handle submit button click event
-  ## TODO - Fix error 
   observeEvent(input$submitBtn, {
     output$tableOutput <- renderDataTable({
       datatable(result(), options = list(scrollX = TRUE))

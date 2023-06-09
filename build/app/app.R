efddd0c1be891e86f47b02850e229167924511a6
Justin Marciszewski <justinjm@google.com>
2023-06-09 16:25:51 -0400

    pseudo code

--- build/app/app.R
+++ build/app/app.R
@@ -37,94 +37,61 @@ custom_google_auth()
 cat(file = stderr(), paste0("Does a gar token exist: ", googleAuthR::gar_has_token()), "\n")
 
 ## UI -----------------------------------------------------------------------
+# Uncomment below lines to install required packages if not already installed
+# install.packages("shiny")
+# install.packages("bigrquery")
+# install.packages("googleCloudVertexAIR")
+
+# Load the necessary packages
+library(shiny)
+library(bigrquery)
+library(googleCloudVertexAIR)
+
+# Start the shiny app
+# shinyApp(
 ui <- fluidPage(
-  div(
-    titlePanel("Vertex AI GenAI App Demo"),
-    style = "color: white; background-color: #4285f4"
-  ),
+  titlePanel("BigQuery and Vertex AI Integration"),
   sidebarLayout(
     sidebarPanel(
-      h3("Welcome to a Demo Shiny App!"),
-      p("This application allows you interact with Vertex AI Generative APIs for text. Fill out the inputs below and then ask a question to the right."),
-      tags$p("Learn more about Generative AI on Vertex AI ", 
-             tags$a(href = "https://cloud.google.com/vertex-ai/docs/generative-ai/learn/overview", target="_blank", "here")
-      ),
-      textInput("user_name", "User Name", "<FIRST NAME, LAST NAME>"),
-      tags$hr(),
-      selectInput("model_name", "Model Name",
-                  choices = c("text-bison"), selected = "text-bison"),
-      tags$hr(),
-      sliderInput("temperature", "Temperature", min = 0.1, max = 1.0, value = 0.2, step = 0.1),
-      sliderInput("max_length", "Maximum Length", min = 1, max = 1024, value = 256, step = 1),
-      tags$hr(),
-      # textAreaInput(inputId = "sysprompt", label = "PROMPT",height = "200px", placeholder = "You are a helpful assistant."),
-      # tags$hr(),
-      tags$div(
-        style="text-align:center; margin-top: 15px; color: white; background-color: #FFFFFF",
-        a(href="https://github.com/justinjm/google-cloud-run-r-shiny", target="_blank",
-          img(src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png", height="30px"),
-          "View source code on Github"
-        )
-      ),
-      style = "background-color: #1a1b1f; color: white"
-    )
-    ,
+      textInput("user_input1", "Enter your text:"),
+      actionButton("submit_button1", "Submit"),
+      verbatimTextOutput("response1"),
+      textInput("user_input2", "Verify response:"),
+      actionButton("submit_button2", "Submit")
+    ),
     mainPanel(
-      tags$style(type = "text/css", ".shiny-output-error {visibility: hidden;}"),
-      tags$style(type = "text/css", ".shiny-output-error:before {content: ' Check your inputs or authentication';}"),
-      tags$style(type = "text/css", "label {font-weight: bold;}"),
-      fluidRow(
-        column(12,tags$h3("Chat History"),tags$hr(),uiOutput("chat_history"),tags$hr())
-      ),
-      fluidRow(
-        column(11,textAreaInput(inputId = "user_message", placeholder = "Enter your message:", label="Prompt", width = "100%")),
-        column(1,actionButton("send_message", "Submit", icon = icon("play"),height = "350px"))
-      ),style = "background-color: #519BF7")
-  ),style = "background-color: #3d3f4e")
-
-
+      textOutput("response2")
+    )
+  )
+)
 ## server ------------------------------------------------------------------
-server <- function(input, output, session) {
-  chat_data <- reactiveVal(data.frame())
-  
-  observeEvent(input$send_message, {
-    if (input$user_message != "") {
-      new_data <- data.frame(source = "User", message = input$user_message, stringsAsFactors = FALSE)
-      chat_data(rbind(chat_data(), new_data))
-      
-      api_res <- gcva_text_gen_predict(
-        projectId = project_id,
-        locationId = region,
-        prompt=input$user_message,
-        modelId=input$model_name,
-        temperature = input$temperature,
-        maxOutputTokens=input$max_length,
-        topP=0.8,
-        topK=40
-      )
-      
-      if (!is.null(api_res)) {
-        api_data <- data.frame(source = "Response:", message = api_res, stringsAsFactors = FALSE)
-        chat_data(rbind(chat_data(), api_data))
-      }
-      updateTextInput(session, "user_message", value = "")
-    }
+server <- function(input, output) {
+  response1_data <- eventReactive(input$submit_button1, {
+    # Put your BigQuery query here, with input$user_input1 as a parameter
+    query <- paste("SELECT * FROM dataset WHERE filter = '", input$user_input1, "'", sep = "")
+    # Assumes you have your authentication set up already
+    response <- bigrquery::bigrquery(query)
+    return(response)
   })
   
-  output$chat_history <- renderUI({
-    chatBox <- lapply(1:nrow(chat_data()), function(i) {
-      tags$div(class = ifelse(chat_data()[i, "source"] == "User", "alert alert-secondary", "alert alert-success"),
-               HTML(paste0("<b>", chat_data()[i, "source"], ":</b> ", chat_data()[i, "message"])))
-    })
-    do.call(tagList, chatBox)
+  output$response1 <- renderPrint({
+    response1_data()
   })
   
-  observeEvent(input$download_button, {
-    if (nrow(chat_data()) > 0) {
-      session$sendCustomMessage(type = "downloadData", message = "download_data")
-    }
+  response2_data <- eventReactive(input$submit_button2, {
+    # Send the response to the Vertex AI
+    response <- googleCloudVertexAIR::predict_text(response1_data(), input$user_input2)
+    return(response)
+  })
+  
+  output$response2 <- renderText({
+    response2_data()
   })
 }
+
+
+
+
 
 ## initialize  -------------------------------------------------------------
 shinyApp(ui = ui, server = server)

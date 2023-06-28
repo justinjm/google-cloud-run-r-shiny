4230a3352559cd0d79b6569ccd57384191879477
Justin Marciszewski <justinjm@users.noreply.github.com>
2023-06-28 12:07:48 -0400

    e2e working login to hide entire app - combine with bq app

--- 07-app-secure-ui-ep.R
+++ 07-app-secure-ui-ep.R
@@ -1,3 +1,14 @@
+library(shiny)
+library(firebase)
+library(gargle)
+library(bigrquery)
+library(DT)
+
+## set and print constants for use below and in logging 
+cat(file = stderr(), "> project_id:", project_id <- Sys.getenv("PROJECT_ID"), "\n")
+cat(file = stderr(), "> region:", region <- Sys.getenv("REGION"), "\n")
+credentials_app_default(scopes="https://www.googleapis.com/auth/cloud-platform")
+
 # firebase modals ---------------------------------------------------
 sign_in <- modalDialog(
   title = "Sign in",
@@ -52,6 +63,43 @@ server <- function(input, output){
     f$req_sign_in()
     ui_secret
   })
+  
+  # Function to query BigQuery and retrieve results
+  queryBigQuery <- function(query) {
+    query_job <- bq_project_query(project_id, query = query)
+    bq_table <- bq_table_download(query_job)
+    return(bq_table)
+  }
+  
+  # Reactive expression to store the query result
+  result <- eventReactive(input$submit_query, {
+    query <- input$queryInput
+    if (!is.null(query) && query != "") {
+      tryCatch({
+        queryBigQuery(query)
+      }, error = function(e) {
+        return(paste("Error:", e$message))
+      })
+    } else {
+      return(data.frame())
+    }
+  })
+  
+  # Render the result in a data table
+  output$tableOutput <- renderDataTable({
+    f$req_sign_in()
+    datatable(result(), options = list(scrollX = TRUE))
+  })
+  
+  # Render error messages
+  output$errorOutput <- renderPrint({
+    f$req_sign_in()
+    result()
+  })
+  
+  
+  
+  
 }
 
 shiny::shinyApp(ui = ui_secure, server = server)
